ccc9a4e7e085d5016130505ad764c43bc63f1b82
Sue Kim <suesunmi@gmail.com>
2012-01-24 16:42:50 -0600

    uses io object to show board

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,12 @@
 class Game
   attr_reader :board
-  def initialize
-    @board = Hash.new("")
+
+  def initialize(io)
+    @board = Hash[:one, "", :two, "", :three, "", :four, "", :five, "", :six, "", :seven, "", :eight, "", :nine, ""]
+    @io = io
+  end
+
+  def show_board
+    @io.board_display(@board)
   end
 end
