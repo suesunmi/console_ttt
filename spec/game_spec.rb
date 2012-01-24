bd090a4d7e34c50bed2c05033d376f1d06b4c4bf
Sue Kim <suesunmi@gmail.com>
2012-01-24 16:41:10 -0600

    uses mock object to test empty board display

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,18 @@
 require 'game'
 
+class MockIO
+  def board_display(board)
+    board.values
+  end
+end
+
 describe Game do
-  it "returns board with 9 plays, all empty" do
-    game = Game.new
-    game.board[:one].should == ""
+  before(:each) do
+    @io = MockIO.new
+    @game = Game.new(@io)
+  end
+
+  it "displays an empty board" do
+    @game.show_board.should == ["", "", "", "", "", "", "", "", ""]
   end
 end
