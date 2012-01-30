4c965f2509a5e188c13a82f2805404b8194d92f5
Sue Kim <suesunmi@gmail.com>
2012-01-30 14:19:18 -0600

    finish game play

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,106 @@
+require_relative 'board'
+require_relative 'player'
+
 class Game
-  attr_reader :board
+  attr_reader :board, :player_a, :player_b, :whose_turn
 
   def initialize(io)
-    @board = Hash[:one, "", :two, "", :three, "", :four, "", :five, "", :six, "", :seven, "", :eight, "", :nine, ""]
-    @io = io
+    @board = Board.new
+    @io= io
+    @player_a = nil
+    @player_b = nil
+    @whose_turn = nil
+    @over = false
+  end
+  
+  def run
+    show_welcome
+    set_up_players
+    start_game
+    play
+  end
+
+  def show_welcome
+    @io.puts "Welcome to Tic Tac Toe"
+  end
+
+  def set_up_players
+    prompt_first_player_name
+    collect_first_player
+    prompt_second_player_name
+    collect_second_player
+  end
+
+  def prompt_first_player_name
+    @io.puts "What is your name?"
+  end
+
+  def collect_first_player
+    name = @io.gets.chomp
+    @player_a = Player.new(name, "X")
+  end
+
+  def prompt_second_player_name
+    @io.puts "What is the name of the other player?"
+  end
+
+  def collect_second_player
+    name = @io.gets.chomp
+    @player_b = Player.new(name, "O")
+  end
+
+  def start_game
+    @whose_turn = @player_a
+    @over = false
+  end
+
+  def play
+    begin 
+      show_board
+      prompt_next_play
+      collect_play
+      evaluate
+    end while over? == false
   end
 
   def show_board
-    @io.board_display(@board)
+    @io.puts @board.display
+  end
+
+  def prompt_next_play
+    @io.puts "#{@whose_turn.name}, enter the position you would like to play:"
+  end
+
+  def collect_play
+    position = @io.gets
+    position = position.to_i if position.class == String
+    @board.plays[position] = @whose_turn.marker
+  end
+
+  def over?
+    @over
+  end
+
+  def evaluate
+    if @board.full? || @board.has_winner then
+      @over = true 
+    end
+    if @over == false then 
+      toggle_player 
+    end
+  end
+
+  def toggle_player
+    if @whose_turn == @player_a then @whose_turn = @player_b
+    elsif @whose_turn == @player_b then @whose_turn = @player_a
+    end
   end
 end
+
+
+#  every puts statement is its own method, for the sake of being able to test the io.
+#  Things I don't like:
+#
+#     prompt text literals
+#     string manipulation of inputs coming from the console
+#     explicit methods for 1st player and 2nd player collection
