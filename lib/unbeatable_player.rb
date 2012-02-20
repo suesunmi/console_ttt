042efaa81bb925f7ace96150e498676bb10ac82e
Sue Kim <suesunmi@gmail.com>
2012-02-20 10:56:38 -0600

    passes all minimax tests

--- lib/unbeatable_player.rb
+++ lib/unbeatable_player.rb
@@ -1,3 +1,4 @@
+
 class UnbeatablePlayer
   attr_accessor :name, :id, :board
 
@@ -5,22 +6,21 @@ class UnbeatablePlayer
     @name = name
     @id = id
     @board = board
+    @tabs = Hash.new
   end
 
   def make_next_play
-puts "#{@id} looking at board:"
-puts @board.display
     opponent = @board.opponent(@id)
     choices = Hash.new
     available = @board.empties
     available.each do |position|
-puts "Evaluating position #{position}"
       test_board = Board.new(@board)
       test_board.play(position, @id)
-
+puts "If #{@id} plays #{position}"
+puts test_board.display
+@tabs[test_board.object_id] = ""
       best = -(best_for(opponent, test_board))
       choices[position] = best
-puts "Overall score for #{position} = #{best}"
     end
     optimal_pos = choices.key(choices.values.max)
     @board.play(optimal_pos, @id)
@@ -30,12 +30,15 @@ puts "Overall score for #{position} = #{best}"
     opponent = board.opponent(player)
     if board.has_winner
       if board.winner == player
+puts @tabs[board.object_id] + "#{player} wins!!!"
         return 100
       end
       if board.winner == opponent
+puts @tabs[board.object_id] + "#{opponent} wins!!!"
         return -100
       end
     elsif board.full?
+puts @tabs[board.object_id] + "Nobody wins!!!"
       return 0
     end
 
@@ -43,14 +46,16 @@ puts "Overall score for #{position} = #{best}"
     available = board.empties
     available.each do |position|
       test_board = Board.new(board)
+tab_string = ""
+(6 - test_board.empties.size).times{ tab_string += "  " }
+@tabs[test_board.object_id] = tab_string
       test_board.play(position, player)
-      best = -(best_for(opponent, test_board) + available.length)
+puts tab_string + "and #{player} plays #{position}"
+      best = -(best_for(opponent, test_board))
+      best += available.length
       choices[position] = best
-puts "If #{player} plays #{position} score is #{best}"
+puts tab_string + "That means the overall score is #{best} for #{player}"
     end
-puts "The best possible score for #{player} on this board"
-puts board.display
-puts "is #{choices.values.max}"
     return choices.values.max
   end
 end
--- spec/unbeatable_player_spec.rb
+++ spec/unbeatable_player_spec.rb
@@ -6,68 +6,98 @@ describe UnbeatablePlayer do
     @board = Board.new
   end
 
-#  it "blocks opponent's win with 3 plays left" do
-#    player = UnbeatablePlayer.new("Computer", "x", @board)
-#    @board.play(3, "x")
-#    @board.play(5, "o")
-#    @board.play(1, "x")
-#    @board.play(2, "o")
-#    @board.play(6, "x")
-#    @board.play(9, "o")
-#    player.make_next_play
-#    @board.player_at(8).should == "x"
-#  end
-#
-#  it "blocks opponent's win with 3 plays left II" do
-#    player = UnbeatablePlayer.new("Computer", "x", @board)
-#    @board.play(1, "x")
-#    @board.play(6, "x")
-#    @board.play(7, "x")
-#    @board.play(2, "o")
-#    @board.play(4, "o")
-#    @board.play(5, "o")
-#    player.make_next_play
-#    @board.player_at(8).should == "x"
-#  end
-#
-#  it "chooses to win with 3 plays left" do
-#    player = UnbeatablePlayer.new("Computer", "x", @board)
-#    @board.play(1, "x")
-#    @board.play(7, "x")
-#    @board.play(9, "x")
-#    @board.play(3, "o")
-#    @board.play(4, "o")
-#    @board.play(5, "o")
-#    player.make_next_play
-#    @board.player_at(8).should == "x"
-#  end
-#
-#  it "blocks opponent from winning with 5 plays left" do
-#    player = UnbeatablePlayer.new("Computer", "x", @board)
-#    @board.play(5, "x")
-#    @board.play(3, "o")
-#    @board.play(8, "x")
-#    @board.play(2, "o")
-#    player.make_next_play
-#    @board.player_at(1).should == "x"
-#  end
-#
-  it "blocks opponent from winning" do
-    player = UnbeatablePlayer.new("Computer", "b", @board)
-    @board.play(1, "a")
-    @board.play(3, "b")
-    @board.play(7, "a")
+  it "blocks opponent's win with 3 plays left" do
+#1: X   |   2: O   |   3: X
+#4:     |   5: O   |   6: X
+#7:     |   8:     |   9: O
+    player = UnbeatablePlayer.new("Computer", "x", @board)
+    @board.play(3, "x")
+    @board.play(5, "o")
+    @board.play(1, "x")
+    @board.play(2, "o")
+    @board.play(6, "x")
+    @board.play(9, "o")
     player.make_next_play
-    @board.player_at(4).should == "b"
+    @board.player_at(8).should == "x"
+  end
+
+  it "blocks opponent's win with 3 plays left II" do
+# 1: X   |   2: O   |   3:
+# 4: O   |   5: O   |   6: X
+# 7: X   |   8:     |   9:
+    player = UnbeatablePlayer.new("Computer", "x", @board)
+    @board.play(1, "x")
+    @board.play(6, "x")
+    @board.play(7, "x")
+    @board.play(2, "o")
+    @board.play(4, "o")
+    @board.play(5, "o")
+    player.make_next_play
+    @board.player_at(8).should == "x"
+  end
+
+  it "blocks the opponent's win with 3 plays left III" do
+#1: X   |   2: O   |   3: X
+#4: O   |   5: O   |   6:
+#7:     |   8: X   |   9:
+    player = UnbeatablePlayer.new("Computer", "x", @board)
+    @board.play(1, "x")
+    @board.play(2, "o")
+    @board.play(3, "x")
+    @board.play(4, "o")
+    @board.play(5, "o")
+    @board.play(8, "x")
+    player.make_next_play
+    @board.player_at(6).should == "x"
+  end
+
+  it "chooses to win with 3 plays left" do
+    player = UnbeatablePlayer.new("Computer", "x", @board)
+    @board.play(1, "x")
+    @board.play(7, "x")
+    @board.play(9, "x")
+    @board.play(3, "o")
+    @board.play(4, "o")
+    @board.play(5, "o")
+    player.make_next_play
+    @board.player_at(8).should == "x"
+  end
+
+  it "blocks opponent from winning with 5 plays left" do
+    player = UnbeatablePlayer.new("Computer", "x", @board)
+    @board.play(5, "x")
+    @board.play(3, "o")
+    @board.play(8, "x")
+    @board.play(2, "o")
+    player.make_next_play
+    @board.player_at(1).should == "x"
+  end
+
+  it "blocks opponent from winning with 6 plays left" do
+    player = UnbeatablePlayer.new("Computer", "o", @board)
+    @board.play(1, "x")
+    @board.play(3, "o")
+    @board.play(7, "x")
+    player.make_next_play
+    @board.player_at(4).should == "o"
   end 
-#
-#  it "choose to win rather than block the opponent" do
-#    player = UnbeatablePlayer.new("Computer", "x", @board)
-#    @board.play(1, "x")
-#    @board.play(2, "o")
-#    @board.play(4, "x")
-#    @board.play(5, "o")
-#    player.make_next_play
-#    @board.player_at(7).should == "x"
-#  end
+
+  it "blocks opponent from winning with 6 plays left II" do
+    player = UnbeatablePlayer.new("Computer", "b", @board)
+    @board.play(3, "a")
+    @board.play(1, "b")
+    @board.play(9, "a")
+    player.make_next_play
+    @board.player_at(6).should == "b"
+  end 
+
+  it "choose to win rather than block the opponent" do
+    player = UnbeatablePlayer.new("Computer", "x", @board)
+    @board.play(1, "x")
+    @board.play(2, "o")
+    @board.play(4, "x")
+    @board.play(5, "o")
+    player.make_next_play
+    @board.player_at(7).should == "x"
+  end
 end
